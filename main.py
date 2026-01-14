7da8050a26e42801ce8b5e8fb1da25a418f87a33
Shihan Huang <shhuang@link.cuhk.edu.hk>
2026-01-14 10:55:02 -0800

    Initialize the project.

---
+++ main.py
@@ -0,0 +1,16 @@
+from ev2gym.models.ev2gym_env import EV2Gym
+from ev2gym.baselines.mpc.V2GProfitMax import V2GProfitMaxOracle
+from ev2gym.baselines.heuristics import ChargeAsFastAsPossible
+
+config_file = "ev2gym/example_config_files/V2GProfitPlusLoads.yaml"
+
+# Initialize the environment
+env = EV2Gym(config_file=config_file,
+              save_replay=True,
+              save_plots=True)
+state, _ = env.reset()
+# agent = V2GProfitMaxOracle(env,verbose=True) # optimal solution
+agent = ChargeAsFastAsPossible() # heuristic
+for t in range(env.simulation_length):
+    actions = agent.get_action(env) # get action from the agent/ algorithm
+    new_state, reward, done, truncated, stats = env.step(actions)  # takes action
